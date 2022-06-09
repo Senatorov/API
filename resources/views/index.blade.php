2ca0ed7dc4031f4cb01201d5378e63ed35cfb8f8
Senatorov <senatorov.nikolay@mail.ru>
2022-06-10 00:54:59 +0300

    html for vue form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,14 @@
 
 @section('content')
 
+<div class="container">
+    <div class="row mt-4">
+        <h1 class="text-center">Table Employees</h1>
 
+        <div class="row mt-3" id="form">
+
+        </div>
+    </div>
+</div>
 
 @endsection
